a06b39c63c7d20d5916b6f6b6b60d7e86044417c
Anton Solback <anton.solback@gmail.com>
2018-11-17 14:49:21 +0100

    Able the same blackboard accross servers

--- server/server.py
+++ server/server.py
@@ -24,10 +24,9 @@ OK = 200
 BAD_REQUEST = 400
 INTERNAL_SERVER_ERROR = 500
 
-JSON_DATA = "application/json"
+JSON_DATA_HEADER = {'content-type':'application/json'}
 
-START_NODE = 1
-
+START_NODE = 4
 # ------------------------------------------------------------------------------------------------------
 try:
     app = Bottle()
@@ -35,35 +34,37 @@ try:
     # Dictionary to represent the board
     board = {}
     # Each entry gets a sequence number
-    entry_number = 1
+    entry_sequence = 1
 
     leader_number = random.randint(0, 1000)
     leader_node = 0
+    leader_ip = ""
 
     def decide_leader():
-        """
-        A method that initiates the leader decision conversation
-        """
+        # ------------------------------------------------------------------------------------------------------
+        # INIT FUNCTIONS
+        # ------------------------------------------------------------------------------------------------------
         global vessel_list, node_id
 
         try:
-            # So that all servers start
-            time.sleep(2)
             # This node starts the conversation starts the conversation"
+            time.sleep(1)
             if node_id == START_NODE:
                 # Acquire IP address of next vessel
-                next_vessel = get_next_ip(vessel_list, str(int(node_id)+1))
-                data_to_send = {
-                    "current_leader_node": node_id,
-                    "current_leader_number": leader_number
+                next_vessel = get_next_ip(str(int(node_id)+1))
+                data = {'current_leader_node': node_id,
+                        'current_leader_number': leader_number
                 }
                 # Make initial request
-                requests.post('http://{}/leader/decide'.format(next_vessel), data=data_to_send)
+                requests.post('http://{}/leader/decide'.format(next_vessel),
+                              headers=JSON_DATA_HEADER, data=json.dumps(data))
 
         except Exception as e:
             print e
 
-    def get_next_ip(vessel_list, next_node_id):
+    def get_next_ip(next_node_id):
+        global vessel_list
+
         try:
             if vessel_list.get(next_node_id, None) is None:
                 next_node_id = "1"
@@ -73,10 +74,9 @@ try:
 
     # ------------------------------------------------------------------------------------------------------
     # BOARD FUNCTIONS
-    # Should nopt be given to the student
     # ------------------------------------------------------------------------------------------------------
     def add_new_element_to_store(entry_sequence, element, is_propagated_call=False):
-        global board, node_id, entry_number
+        global board, node_id
         success = False
         try:
             board[entry_sequence] = element
@@ -109,12 +109,12 @@ try:
     # DISTRIBUTED COMMUNICATIONS FUNCTIONS
     # should be given to the students?
     # ------------------------------------------------------------------------------------------------------
-    def contact_vessel(vessel_ip, path, payload=None, req='POST'):
+    def contact_vessel(vessel_ip, path, payload, req = 'POST', headers=None):
         # Try to contact another server (vessel) through a POST or GET, once
         success = False
         try:
             if 'POST' in req:
-                res = requests.post('http://{}{}'.format(vessel_ip, path), data=payload)
+                res = requests.post('http://{}{}'.format(vessel_ip, path), headers=headers, data=payload)
             elif 'GET' in req:
                 res = requests.get('http://{}{}'.format(vessel_ip, path))
             else:
@@ -127,78 +127,67 @@ try:
             print e
         return success
 
-    def propagate_to_vessels(path, payload = None, req = 'POST'):
+    def propagate_to_vessels(path, payload = None, headers=None, req = 'POST'):
         global vessel_list, node_id
 
         for vessel_id, vessel_ip in vessel_list.items():
             if int(vessel_id) != node_id: # don't propagate to yourself
-                success = contact_vessel(vessel_ip, path, payload, req)
+                success = contact_vessel(vessel_ip, path, payload, req=req, headers=headers)
                 if not success:
                     print "\n\nCould not contact vessel {}\n\n".format(vessel_id)
 
-    def _begin_propagation(url, entry=None):
+    def _begin_propagation(url, payload=None, headers=None):
         """
         Since there are muliple places where we want to start a new
         thread, just extract that functionality here to avoid duplication
         """
-        thread = Thread(target=propagate_to_vessels, args=(url, entry))
+        thread = Thread(target=propagate_to_vessels, args=(url, payload, headers))
         thread.daemon = True
         thread.start()
 
 
     # ------------------------------------------------------------------------------------------------------
-    # ROUTES
+    # ROUTESw
     # ------------------------------------------------------------------------------------------------------
     # a single example (index) should be done for get, and one for post
     # ------------------------------------------------------------------------------------------------------
     @app.route('/')
     def index():
-        global board, node_id, vessel_list
-        """
-        TODO
-        
-        We need to ask the leader here what the current board is and update it
-        """
-        board = requests.get("http://{}/askleader/getboard".format(vessel_list[str(leader_node)])).json()
-        # Sort dictionary when sending it, since when looping through a dictionary the order is not preserved
-        return template('server/index.tpl', board_title='Vessel {}'.format(node_id), board_dict=sorted(board.items(), key=operator.itemgetter(0)), members_name_string='Anton Solback')
+        global board, node_id, leader_node
+
+        try:
+            return template('server/index.tpl', board_title='Vessel {}'.format(node_id),
+                            board_dict=sorted(board.items(), key=operator.itemgetter(0)), members_name_string='Anton Solback')
+        except Exception as e:
+            print e
+            response.status = INTERNAL_SERVER_ERROR
 
     @app.get('/board')
     def get_board():
         global board, node_id
-        # Sort dictionary when sending it, since when looping through a dictionary the order is not preserved
-        return template('server/boardcontents_template.tpl',board_title='Vessel {}'.format(node_id), board_dict=sorted(board.items(), key=operator.itemgetter(0)))
+
+        try:
+            return template('server/boardcontents_template.tpl', board_title='Vessel {}'.format(node_id),
+                            board_dict=sorted(board.items(), key=operator.itemgetter(0)))
+        except Exception as e:
+            print e
+            response.status = INTERNAL_SERVER_ERROR
     # ------------------------------------------------------------------------------------------------------
     @app.post('/board')
     def client_add_received():
-        '''
-        Adds a new element to the board
-        Called directly when a user is doing a POST request on /board
-        '''
-        global board, node_id, entry_number, vessel_list
+        global board, node_id, entry_sequence, vessel_list
+
         try:
             entry = request.forms.get('entry')
-            # Set the default response status
-            response.status = BAD_REQUEST
+            # Leader should only talk to other servers
+            if node_id != leader_node:
+                data_to_send = json.dumps({
+                    "entry": entry
+                })
+                requests.post("http://{}/askleader/{}".format(leader_ip, BOARD_ADD),
+                              headers=JSON_DATA_HEADER, data=data_to_send)
 
-            if add_new_element_to_store(entry_number, entry):
-
-                """
-                First naive solution
-                Client adds new message -> this vessel sends message to leader -> 
-                leader determines order -> leader sends back current board -> this vessel updates board -> 
-                client will see correct board
-                """
-                # Start new thread to propagate
-                print "http://{}/askleader/{}".format(vessel_list[str(leader_node)], BOARD_ADD)
-                res = requests.post("http://{}/askleader/{}".format(vessel_list[str(leader_node)], BOARD_ADD), data=entry)
-
-                print res.json()
-                board = res.json()
-
-                entry_number += 1
-                # We successfully added an item, change response code
-                response.status = OK
+            response.status = 200
 
         except Exception as e:
             print e
@@ -206,52 +195,43 @@ try:
 
     @app.post('/board/<element_id:int>/')
     def client_action_received(element_id):
-        # Receives an action to perform on the board
+        global board, node_id
         try:
-            action_to_perform = request.forms.get('action')
+            action_to_perform = request.forms.get("action")
             entry = request.forms.get("entry")
-            # Set the default response status
+
             response.status = BAD_REQUEST
 
-            # Modify
-            if action_to_perform == "0":
-                if modify_element_in_store(element_id, entry):
-                    # Start new thread to propagate
-                    _begin_propagation("/propagate/{}/{}".format(BOARD_MODIFY, element_id), entry)
-                    response.status = OK
-            # Delete
-            else:
-                if delete_element_from_store(element_id):
-                    # Start new thread to propagate
-                    _begin_propagation("/propagate/{}/{}".format(BOARD_DELETE, element_id))
-                    response.status = OK
+            if node_id != leader_node:
+                if action_to_perform == "0":
+                    data_to_send = json.dumps({
+                            "entry_sequence": element_id,
+                            "entry": entry
+                    })
+                    requests.post("http://{}/askleader/{}".format(leader_ip, BOARD_MODIFY),
+                                          headers=JSON_DATA_HEADER, data=data_to_send)
+                else:
+                    data_to_send = json.dumps({
+                        "entry_sequence": element_id
+                    })
+                    requests.post("http://{}/askleader/{}".format(leader_ip, BOARD_DELETE),
+                                          headers=JSON_DATA_HEADER, data=data_to_send)
+
+            response.status = 200
 
         except Exception as e:
             print e
             response.status = INTERNAL_SERVER_ERROR
 
-    @app.post('/propagate/<action>/<element_id:int>')
-    def propagation_received(action, element_id):
-        global entry_number
+    @app.post('/propagate/<action>')
+    def propagation_received(action):
+        global entry_sequence, board, node_id, leader_node
 
         try:
-            entry = request.body.getvalue()
-            # Set the default response status
-            response.status = BAD_REQUEST
+            data = request.json
 
-            # If the acton was successful then set the response code to indicate this. Applies for all actions
             if action == BOARD_ADD:
-                if add_new_element_to_store(element_id, entry, True):
-                    entry_number += 1
-                    response.status = OK
-
-            elif action == BOARD_DELETE:
-                if delete_element_from_store(element_id, True):
-                    response.status = OK
-
-            else:
-                if modify_element_in_store(element_id, request.body.getvalue(), True):
-                    response.status = OK
+                add_new_element_to_store(data["entry_sequence"], data["entry"], True)
 
         except Exception as e:
             print e
@@ -265,39 +245,41 @@ try:
         Route used to decide who the leader is
         :return:
         """
-        global vessel_list, node_id, leader_node
+        global vessel_list, node_id, leader_node, leader_ip
 
         try:
+            payload = request.json
+
             if action == "confirm":
-                leader_node = request.forms.get("leader_node")
-                print "Leader node: " + leader_node
+                leader_node = payload["leader_node"]
+                leader_ip = payload["leader_ip"]
+                print "Leader node:", leader_node
             else:
-                current_leader_node = request.forms.get("current_leader_node")
-                current_leader_number = request.forms.get("current_leader_number")
+                current_leader_node = payload["current_leader_node"]
+                current_leader_number = payload["current_leader_number"]
 
                 if node_id == START_NODE:
-                    # We have visited all vessels, propagate message to other servers that they should confirm leader node
-                    _begin_propagation("/leader/confirm", {"leader_node":current_leader_node})
+                    leader_ip = vessel_list[str(current_leader_node)]
                     leader_node = current_leader_node
+                    # We have visited all vessels, propagate message to other servers that they should confirm leader node
+                    _begin_propagation("/leader/confirm", json.dumps({"leader_node":leader_node, "leader_ip": leader_ip}),
+                                       headers=JSON_DATA_HEADER)
                 else:
                     # Send message to next vessel
-                    next_vessel = get_next_ip(vessel_list, str(int(node_id)+1))
+                    next_vessel = get_next_ip(str(int(node_id)+1))
 
                     if leader_number > int(current_leader_number):
                         new_leader_node = node_id
                         new_leader_number = leader_number
 
-                        data_to_send = {
+                        payload = {
                             "current_leader_node": new_leader_node,
                             "current_leader_number": new_leader_number
                         }
-                    else:
-                        data_to_send = {
-                            "current_leader_node": current_leader_node,
-                            "current_leader_number": current_leader_number
-                        }
 
-                    requests.post('http://{}/leader/decide'.format(next_vessel), data=data_to_send)
+                    requests.post('http://{}/leader/decide'.format(next_vessel),
+                                  headers=JSON_DATA_HEADER, data=json.dumps(payload))
+            response.status = 200
 
         except Exception as e:
             print e
@@ -311,35 +293,31 @@ try:
     # Execute the code
     @app.post('/askleader/<action>')
     def ask_leader(action):
-        global entry_number
+        global entry_sequence
 
         try:
+            data = request.json
+
             if action == BOARD_ADD:
-                add_new_element_to_store(entry_number, request.body.getvalue())
-                entry_number += 1
+                add_new_element_to_store(entry_sequence, data["entry"])
+                payload = {
+                    "entry_sequence": entry_sequence,
+                    "entry": data["entry"]
+                }
+                _begin_propagation("/propagate/{}".format(BOARD_ADD), json.dumps(payload), JSON_DATA_HEADER)
+                entry_sequence += 1
 
+            elif action == BOARD_MODIFY:
+                modify_element_in_store(int(data["entry_sequence"]), data["entry"])
+            else:
+                delete_element_from_store(data["entry_sequence"])
 
-            return HTTPResponse(
-                status=200,
-                body=json.dumps(board),
-                content_type=JSON_DATA
-            )
+            response.status = OK
 
         except Exception as e:
             print e
             response.status = INTERNAL_SERVER_ERROR
 
-    @app.get('/askleader/getboard')
-    def return_board():
-        try:
-            return HTTPResponse(
-                status=200,
-                body=json.dumps(board),
-                content_type=JSON_DATA
-            )
-        except Exception as e:
-            print e
-            response.status = INTERNAL_SERVER_ERROR
     # ------------------------------------------------------------------------------------------------------
 
     # ------------------------------------------------------------------------------------------------------
--- test_solution.zsh
+++ test_solution.zsh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 
+targets=('10.1.0.1' '10.1.0.2' '10.1.0.3' '10.1.0.4' '10.1.0.5' '10.1.0.6' '10.1.0.7' '10.1.0.8')
 index=1
 
-for ((i = 1; i <= 8; i++)); do
-	curl -s -X POST -F 'entry=test.'$index http://10.1.0.$i:80/board > /dev/null
-	((index++))
+for ip_address in $targets; do
+	for ((i = 1; i < 4; i++)); do
+		curl -s -X POST -F 'entry=test.'$index http://$ip_address:80/board > /dev/null
+		((index++))
+	done
 done
